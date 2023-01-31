3858e0229109dae34bdc9f5aed566f256a3ab853
Z1n <tv.noob@bk.ru>
2023-01-31 22:36:53 +0700

    create form for note

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let eventBus = new Vue()
 
 Vue.component('first-col', {
     template: `
@@ -5,6 +6,7 @@ Vue.component('first-col', {
             <note></note>    
         </div>
     `,
+    
 })
 
 Vue.component('second-col', {
@@ -28,15 +30,6 @@ Vue.component('note', {
         return {
             notes: [
                 {
-                    title: 'title1',
-                    points: [
-                        'dsad',
-                        'dasdsa',
-                        'qwe',
-                    ],
-                    type: 'col-1',
-                },
-                {
                     title: 'title2',
                     points: [
                         'dsad',
@@ -44,16 +37,7 @@ Vue.component('note', {
                         'qwe',
                     ],
                     type: 'col-2',
-                },
-                {
-                    title: 'title3',
-                    points: [
-                        'dsad',
-                        'dasdsa',
-                        'qwe',
-                    ],
-                    type: 'col-3',
-                },
+                }
             ]
         }
     },
@@ -62,11 +46,67 @@ Vue.component('note', {
             <div class="m-3 p-3 border border-danger" v-for="note in notes">
                 <h5>{{note.title}}</h5>
                 <ul>
-                    <li v-for="point in note.points">{{ point }}</li>
+                    <li v-for="point in note.points">
+                        {{ point }}
+                    </li>
                 </ul>
             </div>
         </div>
     `,
+    mounted() {
+        eventBus.$on('note-created', note => {
+            this.notes.push(note)
+        })
+    }
+})
+
+Vue.component('create-note', {
+    data() {
+        return {
+            title: '',
+            points: '',
+            type: '',
+            test: ['', '', 'eqwe', 'qwewqe']
+        }
+    },
+    template: `
+        <div class="d-flex justify-content-center">
+            <form class="d-flex flex-column w-50 mt-4" @submit.prevent="createNote">
+                <input class="form-control mb-3" type="text" placeholder="Заголовок" v-model="title">
+                <div class="form-floating mb-3">
+                    <textarea class="form-control" placeholder="Напишите здесь ваши заметки" id="textarea" style="height: 200px; resize: none;" v-model="points"></textarea>
+                    <label for="textarea">Каждый пункт писать с новой строки!</label>
+                </div>
+                <input class="btn btn-primary" type="submit" value="Создать">
+            </form>
+        </div>
+    `,
+    methods: {
+        filterNote() {
+            let newTest = []
+            console.log(typeof(this.test))
+            for (i in this.test) {
+                console.log(this.test[i])
+                if (this.test[i].lenght != 0) {
+                    
+                }
+            }
+            console.log(newTest)
+        },
+        createNote() {
+            if (this.title && this.points) {
+                let note = {
+                    title: this.title,
+                    points: this.points.split("\n"),
+                    type: 'col-1'
+                }
+                this.filterNote()
+                eventBus.$emit('note-created', note)
+                this.title = '',
+                this.points = ''
+            }
+        }
+    }
 })
 
 let app = new Vue({
